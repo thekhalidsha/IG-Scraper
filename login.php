6d090f97b77eecefb37028ca4e7c21ca81f0b7f2
Cyber Jeevi <72810866+cyberjeevi@users.noreply.github.com>
2021-06-20 16:09:55 +0530

    Add files via upload

---
+++ login.php
@@ -0,0 +1,25 @@
+<?php
+
+
+//Put Username in between the "" (double quotes)
+
+$userd = "";
+
+
+
+//Put Password in between the "" (double quotes)
+
+$paasword = "";
+
+
+
+
+//target usernames 
+// Insert Usernames as an array!! 
+// Must be String and Be TYPE OF ARRAY
+
+$items = array("team_kalimayam", "team__haash", "cristiano", "dqsalmaan", "therock", "selenagomez", "sonurajputsr", "bikergram_kerala", "bikergram_kerala_", "ktm_india", "ktmkerala_");
+
+
+
+?>
